01a391f69403a3ba2a802a7471977d8b24ff6178
Michael Mullen <mullen326@gmail.com>
2015-04-11 14:57:27 -0700

    Actual Vector4 classes

--- Vector4.cpp
+++ Vector4.cpp
@@ -7,3 +7,90 @@
 //
 
 #include "Vector4.h"
+
+
+Vector4::Vector4()
+{
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
+    w = 0.0;
+}
+
+
+Vector4::~Vector4()
+{
+}
+
+Vector4::Vector4(double x, double y, double z, double w)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->w = w; //pass one array and pass one pointer ver
+}
+
+Vector4 Vector4::operator+(const Vector4& v)
+{
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    
+    return *this;
+}
+
+float Vector4::length()
+{
+    return sqrt(x*x + y*y + z*z);
+}
+
+void Vector4::normalize()
+{
+    float l = this->length();
+    x = x/l;
+    y=y/l;
+    z=z/l;
+    
+}
+
+Vector4 Vector4::operator-(const Vector4& v)
+{
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    
+    return *this;
+}
+void Vector4::dehomogenize()
+{
+    //w -> 1 turns on translation else w -> 0 off translation
+    x = x / w;
+    y = y / w;
+    z = z / w;
+    w = w / w;
+}
+
+
+
+double Vector4::dot(const Vector4& v1, const Vector4& v2)
+{
+    return ((v1.x*v2.x) + (v1.y*v2.y) + (v1.z*v2.z));
+}
+
+Vector4 Vector4::cross(const Vector4& v1, const Vector4& v2)
+{
+    
+    this->x = (v1.y*v2.z) - (v1.z*v2.y);
+    this->y = (v1.z*v2.x) - (v1.x*v2.z);
+    this->z = (v1.x*v2.y) - (v1.y*v2.x);
+    
+    return *this;
+}
+
+
+void Vector4::scale(double s)
+{
+    x = s*x;
+    y = s*y;
+    z = s*z;
+}
--- Vector4.h
+++ Vector4.h
@@ -10,6 +10,34 @@
 #define __cse169__Vector4__
 
 #include <stdio.h>
+#include <string>
+#include <math.h>
+
+#define M_PI 3.14159265358979323846
+class Vector4
+{
+public:
+    double x;
+    double y;
+    double z;
+    double w;
+    Vector4();
+    ~Vector4();
+    Vector4(double x, double y, double z, double w);
+    Vector4 operator+(const Vector4& v);
+    Vector4 operator-(const Vector4& v);
+    void dehomogenize();
+    void print(std::string comment);
+    float length();
+    void normalize();
+    double dot(const Vector4& v1, const Vector4& v2);
+    Vector4 cross(const Vector4& v1, const Vector4& v2);
+    
+    void scale(double s);
+    
+    
+};
+
 
 
 #endif /* defined(__cse169__Vector4__) */
